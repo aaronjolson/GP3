acc801f103684ec81a557d89ed425fedb729a01f
Aaron Olson <aaron.olson@agosto.com>
2016-11-27 13:28:43 -0600

    refactor.

--- src/AccelerateRequestManager.java
+++ src/AccelerateRequestManager.java
@@ -1,35 +1,24 @@
-
-/**
- * 
- * @author Brahma Dathan and Sarnath Ramnath
- * @Copyright (c) 2010
- 
- * Redistribution and use with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *   - the use is for academic purpose only
- *   - Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   - Neither the name of Brahma Dathan or Sarnath Ramnath
- *     may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- * The authors do not make any claims regarding the correctness of the code in this module
- * and are not responsible for any loss or damage resulting from its use.  
- */
 import java.util.EventListener;
-
 import javax.swing.event.EventListenerList;
 
+/**
+ * Class for managing the acceleration requests and listeners.
+ */
 public class AccelerateRequestManager {
 	private EventListenerList listenerList = new EventListenerList();
 	private static AccelerateRequestManager instance;
 
-	private AccelerateRequestManager() {
-	}
+  /**
+   *  Private constructor to keep the class private
+   */
+  private AccelerateRequestManager() {
+  }
 
-	public static AccelerateRequestManager instance() {
+  /**
+   * for creating as a singleton.
+   * @return the instance
+   */
+  public static AccelerateRequestManager instance() {
 		if (instance == null) {
 			instance = new AccelerateRequestManager();
 		}
