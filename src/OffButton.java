143556ed087c42530dfadce7ed96aa9d08dd5a63
Aaron Olson <aaron.olson@agosto.com>
2016-12-01 19:04:52 -0600

    fixing comments

--- src/OffButton.java
+++ src/OffButton.java
@@ -1,15 +1,14 @@
 public class OffButton extends GUIButton {
   /**
    * The button for accelerating
-   *
-   * @param string
+   * @param string the label
    */
   public OffButton(String string) {
     super(string);
   }
 
   /**
-   * Creates a OffRequestEvent, so the interested states can get it. Call the
+   * Creates an OffRequestEvent, so the interested states can get it. Call the
    * manager, so it can forward it
    */
   public void inform(VehicleDisplay source) {
