c2e7efc6375a7b816b039fc12855fed5032b84c5
Aaron Olson <aaron.olson@agosto.com>
2016-12-01 19:46:14 -0600

    fixing comments

--- src/OnRequestListener.java
+++ src/OnRequestListener.java
@@ -2,13 +2,12 @@ import java.util.EventListener;
 
 /**
  * OnRequestListener to process on requests
- *
  */
 public interface OnRequestListener extends EventListener {
   /**
    * Processes on requests
    *
-   * @param event
+   * @param event whatever we get
    */
   public void onRequested(OnRequestEvent event);
 }
