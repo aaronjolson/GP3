e582ea9faf0299a6898e39143528bd610b5b7ff4
Aaron Olson <aaron.olson@agosto.com>
2016-12-01 19:35:35 -0600

    fixing comments

--- src/OffRequestManager.java
+++ src/OffRequestManager.java
@@ -11,6 +11,10 @@ public class OffRequestManager {
   private OffRequestManager() {
   }
 
+  /**
+   * for creating as a singleton.
+   * @return the instance
+   */
   public static OffRequestManager instance() {
     if (instance == null) {
       instance = new OffRequestManager();
@@ -18,14 +22,26 @@ public class OffRequestManager {
     return instance;
   }
 
+  /**
+   * For adding the listener when states change
+   * @param listener the listener to change
+   */
   public void addOffRequestListener(OffRequestListener listener) {
     listenerList.add(OffRequestListener.class, listener);
   }
 
+  /**
+   * For removing the listener when states change
+   * @param listener the listener to change
+   */
   public void removeOffRequestListener(OffRequestListener listener) {
     listenerList.remove(OffRequestListener.class, listener);
   }
 
+  /**
+   * For processing the off request events
+   * @param event the event that made the request
+   */
   public void processEvent(OffRequestEvent event) {
     EventListener[] listeners = listenerList.getListeners(OffRequestListener.class);
     for (int index = 0; index < listeners.length; index++) {
